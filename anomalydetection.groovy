c6b92e17a542646d297f92c41ddcc936d22cd784
Samit Sasan <sasansamit@gmail.com>
2013-12-13 18:16:29 +0530

    updated groovy script for anomaly detection

--- anomalydetection.groovy
+++ anomalydetection.groovy
@@ -1,10 +1,9 @@
 import org.apache.log4j.Logger;
 
+Logger log = Logger.getLogger("anomalydetection.groovy")
 
 //constants to be used/configured
 //time window is 30 seconds
-
-Logger log = Logger.getLogger("anomalydetection.groovy")
 
 def timeWindow = 30*1000 //5*60*1000
 def numEntriesToTriggerFlag = 5
@@ -31,15 +30,36 @@ if (curMemory > memoryThreshold)
 	tsList.add(curTs)
 }
 
-log.info "list size " + tsList.size()
 if (tsList.size() == 0)
 {	
 	log.info "Returning because list is empty";
 	return
 }
+
+if (curMemory < memoryThreshold && ((curTs - tsList.max()) >  timeWindow))
+{
+	if (state["flagstatus"] == "true")
+	{
+		log.info "${curMemory} | ${tsList} | ${curTs} | " + tsList.min()
+		
+		log.info "taking down the flag for device: " + input["deviceid"]
+		state["flagstatus"] = "false"
+		
+		tsList.clear()
+		dataStore.storeData(input["deviceid"], state)
+		
+		return
+	}
+}
+
 //remove all entries which are older than the time window required
 def maxvar = tsList.max()
 tsList.removeAll {maxvar - it > timeWindow}
+
+if (tsList.size() == 6)
+{
+	tsList = tsList - tsList.min()
+}
 
 state["tsList"] = tsList;
 //if 5 entries remain, we have problem
@@ -50,16 +70,7 @@ if ((tsList.size() >= numEntriesToTriggerFlag) && (state["flagstatus"] == "false
 	log.info "raising flag for device: " + input["deviceid"]
 	state["flagstatus"] = "true"
 }
-else if (tsList.size() < numEntriesToTriggerFlag)
-{	
-	if (state["flagstatus"] == "true")
-	{
-		log.info "${curMemory} | ${tsList} | ${curTs} | " + tsList.min()
-		
-		log.info "taking down the flag for device: " + input["deviceid"]
-		state["flagstatus"] = "false"
-	}
-}
+
 
 dataStore.storeData(input["deviceid"], state)
 
